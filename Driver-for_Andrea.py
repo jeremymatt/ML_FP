be8728b77fa6ad5825a5c223f703ae3a8b3a33a8
jeremymatt <jmatt@UVM.EDU>
2019-12-10 12:01:41 -0500

    Comments and remove pair generation code

--- Driver-for_Andrea.py
+++ Driver-for_Andrea.py
@@ -23,94 +23,11 @@ mpl.rcParams['font.size'] = 18
 mpl.rcParams['axes.grid'] = True
 
 
-
-#Initialize the data object:
-ALLdata = LD()
-
-#Location of the data to be loaded
-destdir = './DataBinned/HRRR_INL-5minraw_noS35_no-HRRR/'
-#load pre-binned data
-LoadType = 'binned'
-
-#Load the data
-ALLdata.LoadData(LoadType,destdir)
-#Convert the times to datetime objects
-ALLdata.ConvertTimeStrToDatetime()
-
-#Add variables for the U and V components of wind
-ALLdata.calc_wind_u_v(scale_by_speed=True)
-
-#Load the northing/easting data in to the data objects for each station
-filename = 'northing_easting.xlsx'
-ALLdata.load_xyz(filename)
-
-#Specify the start and end times
-start = '2017-01-01 11:00:00'
-end = '2017-02-01 13:00:00'
-
-start = pd.to_datetime(start)
-end= pd.to_datetime(end)
-
-start_hour = 11
-end_hour = 12
-
-
-data = ALLdata.WSdata[0].data_binned.copy()
-data['hr'] = data['datetime_bins'].dt.hour
-
-m1 = data['datetime_bins']>start
-m2 = data['datetime_bins']<end
-m3 = data['hr']>=start_hour
-m4 = data['hr']<=end_hour
-
-
-
-times_to_process = list(data.loc[m1&m2&m3&m4,'datetime_bins'].values)
-times_to_process = data.loc[m1&m2&m3&m4,'datetime_bins']
-num_times = len(times_to_process)
-
-
-#Specify the list of stations to load
-#ALLdata.WSdata is a list of objects that hold the data for each station
-#Another valid call would be something like ALLdata.WSdata[:10], which would
-#load the data for the first 10 stations
-stations = ALLdata.WSdata 
-
 x = 'easting'
 y = 'northing'
-variables = ['solar:','temp:','speed:','dir:','u','v']
 
-
-generate = False
-if generate:
-    paired_data = pd.DataFrame()
-    
-    for ind,cur_time in enumerate(times_to_process):
-        tic = time.time()
-        #Generate a dataframe holding the data between the start and end times
-        data = ALLdata.get_krig_data(stations,cur_time,cur_time)
-        
-        pairs = GSF.extract_pairs(data, x, y, variables)
-        
-        paired_data = paired_data.append(pairs)
-        
-        toc = time.time()
-        
-        
-        PTR.predict(tic,toc,ind,num_times)
-     
-    half_length = int(paired_data.shape[0]/2)
-    paired_data.reset_index(inplace=True,drop=True)
-    paired_data.iloc[:half_length,:].to_excel('jan2017_midday_pairs_part1.xlsx')
-    paired_data.iloc[half_length:,:].to_excel('jan2017_midday_pairs_part2.xlsx')
-
-paired_data = pd.read_excel('jan2017_midday_pairs_part1.xlsx')
-part2 = pd.read_excel('jan2017_midday_pairs_part2.xlsx')
-
-paired_data = paired_data.append(part2)
-
-
-#paired_data = pd.read_excel('jan2017_11AM_pairs.xlsx')
+#Load the paired head/tails of all parameters
+paired_data = pd.read_excel('jan2017_13_pairs.xlsx')
 #%% Run the SV bin generation code
 #Generate variable names to grab the correct columns from the pairs structure
 primary = 'speed:'
@@ -131,19 +48,12 @@ labels = {}
 labels['xlabel'] = '$\Delta$ Distance (km)'
 labels['ylabel'] = 'Semivariance'
 
-#Set the binning mode type:
-    #Bins of an equal distance width
-    #The number of bins equal to the square root of the number of points
-    
+#Pre-defined bin widths
 bins = {}    
 bins['temp:'] = [0.8063474494709624, 3.2060273344413943, 6.042012653042811, 8.659845254828738, 11.05952513979917, 14.113663175216082, 16.73149577700201, 18.69487022834145, 20.440091962865402, 29.166200635485154, 37.23785115765842, 44.65504352938522, 53.38115220200496, 59.70758098965428, 69.742605963167, 76.7234929012628, 81.95915810483464, 88.28558689248396, 94.17571024650229, 100.7202917509671, 105.30149880409249, 112.50053845900376, 118.82696724665308, 126.02600690156439, 132.5705884060292, 139.11516991049402, 144.56898783088135, 153.2950965035011, 158.96706714070393, 165.51164864516875]
- 
 bins['solar:'] = [0.8063474494709624, 3.2060273344413943, 6.042012653042811, 8.659845254828738, 11.05952513979917, 14.113663175216082, 16.73149577700201, 18.69487022834145, 20.440091962865402, 29.166200635485154, 37.23785115765842, 44.65504352938522, 53.38115220200496, 59.70758098965428, 69.742605963167, 76.7234929012628, 81.95915810483464, 88.28558689248396, 94.17571024650229, 100.7202917509671, 105.30149880409249, 112.50053845900376, 118.82696724665308, 126.02600690156439, 132.5705884060292, 139.11516991049402, 144.56898783088135, 153.2950965035011, 158.96706714070393, 165.51164864516875]
-
 bins['u'] = [0.3865308314618119, 5.346897149008242, 9.22892296274022, 13.75795307876087, 17.424310791729972, 21.73767280698773, 25.619698620719717, 31.011401139791914, 34.246422651235235, 38.34411656573012, 44.1671552863281, 48.91185350311163, 54.08788792142094, 57.75424563439003, 64.65562485880245, 75.65469799770975, 88.16344784195725, 98.51551667857588, 109.51458981748318, 117.9256457472358, 125.68969737469978, 137.55144291665863, 150.49152896243191, 161.2749340005763, 161.6642280031448]
-    
 bins['v'] = [0.3865308314618119, 5.346897149008242, 9.22892296274022, 13.75795307876087, 17.424310791729972, 21.73767280698773, 25.619698620719717, 31.011401139791914, 34.246422651235235, 38.34411656573012, 44.1671552863281, 48.91185350311163, 54.08788792142094, 57.75424563439003, 64.65562485880245, 75.65469799770975, 88.16344784195725, 98.51551667857588, 109.51458981748318, 117.9256457472358, 125.68969737469978, 137.55144291665863, 150.49152896243191, 161.2749340005763, 161.6642280031448]
-
 bins['speed:'] =  [1.4608055999174425, 2.9878746176258986, 4.73309635214985, 7.132776237120282, 9.314303405275218, 12.150288723876642, 14.331815892031578, 16.73149577700201, 19.567481095603426, 22.185313697389354, 25.239451732806266, 28.29358976822318, 30.9114223700091, 33.96556040542601, 35.92893485676546, 37.892309308104906, 40.29198919307533, 43.782432662123234, 45.527654396647186, 47.27287613117114, 48.799945148879594, 51.417777750665515, 53.16299948518947, 55.7808320869754, 57.744206538314835, 59.70758098965428, 61.67095544099373, 63.63432989233318, 68.21553694545855, 73.0148967153994, 81.74100538801915, 94.39386296331779, 105.30149880409249, 120.13588354754606, 134.97026829099963, 149.36834760082223, 161.80305245930538] 
   
 sv_mode = {'bins':bins[primary],'mode':'user.div'}
@@ -155,16 +65,18 @@ sv_output = GSF.generate_semivariogram(differences,labels,sv_mode,
                                        plot_raw = False)
 N = differences.shape[0]
 
+#Plot the skewness of each bin
 plt.figure()
 plt.plot(sv_output['x'],sv_output['skew'])
-
 plt.xlabel('distance (km)')
 plt.ylabel('skew')
 plt.title(primary)
 plt.tight_layout()
 
-bin_divs = pd.Series(sv_output['bins'])
-bin_divs.to_excel('bins_mid-day_{}.xlsx'.format(primary.split(':')[0]))
+save_bins = False
+if save_bins:
+    bin_divs = pd.Series(sv_output['bins'])
+    bin_divs.to_excel('bins_mid-day_{}.xlsx'.format(primary.split(':')[0]))
 
 
 
@@ -333,7 +245,7 @@ if primary == 'v':
     GSF.plot_model_fit(sv_output,model_points,labels,title,ylim=ylim)
 
 
-#%%
+#%% Run the kriging code
 interp_points = pd.read_excel('interp_points_with_stations.xlsx')
 target = list(zip(interp_points[x],interp_points[y]))
 
@@ -358,27 +270,36 @@ Z,EV,weights = OK.estimate(target)
 
 
 
-
+#Add the predicted and the error variance values to the interpolation points
+#dataframe
 interp_points[primary] = Z
 interp_points['{}|EV'.format(primary)] = EV
 
+#Select the line to plot the predicted values and error variance and mask off
+#the relevant data
 line_to_plot = 'AD'
 mask = interp_points['line']==line_to_plot
 to_plot = interp_points.loc[mask,:]
 
+#Fill out labels dict for plotting function
 labels = {}
 labels['xlabel'] = 'Distance along line (km)'
 labels['ylabel'] = '{}'.format(primary.split(':')[0])
 labels['data_label'] = 'Kriged {}'.format(primary.split(':')[0])
 labels['title'] = 'Kriged values along line {}'.format(line_to_plot)
 labels['filename'] = 'kriged_{}_line-{}.png'.format(primary.split(':')[0],line_to_plot)
+#x-variable to plot
 plot_x = 'line_dist'
+#y-variable to plot
 plot_y = primary
+#second y variable to plot
 plot_y2 = '{}|EV'.format(primary)
 
+#generate the plot along the line
 ax1,ax2 = GSF.plot_model_1D(to_plot,plot_x,plot_y,plot_y2,labels)
 
-
+#Plot a figure of the points and stations with the line of interested 
+#highlighted
 plt.figure()
 station_mask = interp_points['type'] == 'station'
 off_line_points = interp_points.loc[(~mask)&(~station_mask),:]
@@ -400,6 +321,11 @@ plt.xlabel('Easting (km)')
 plt.legend()
 
 
+#leave-one-out cross validation of the model:
+#For each station:
+#   1.  Leave station out of kriging and use other stations to interpolate to 
+#   left-out station
+#   2.  calculate error as predicted-actual
 num_stations = data.shape[0]
 errors = []
 predicted = []
@@ -415,10 +341,15 @@ for i in range(num_stations):
     predicted.append(Z)
     errors.append(Z[0]-actual)
 
+
 errors = np.array(errors)
+#Calculate the mean absolute error
 MAE = np.abs(errors).mean()
+#Calculate the mean error
 ME = errors.mean()
 
+
+#Plot a figure showing the error for each station for the current timestep
 plt.figure()
 
 #set plotting defaults
